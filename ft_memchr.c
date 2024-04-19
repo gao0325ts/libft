fb83da8326b35c4db653af905a82264a7d874cd0
stakada <ts20040325@gmail.com>
2024-04-19 11:24:40 +0900

    集中力切れた

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 03:55:34 by stakada           #+#    #+#             */
-/*   Updated: 2024/04/18 00:27:55 by stakada          ###   ########.fr       */
+/*   Updated: 2024/04/19 10:58:33 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,9 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	str = (const char *)s;
 	while (i < n)
 	{
-		while (*str)
-		{
-			if (*str == (unsigned char)c)
-				return ((void *)str);
-			str++;
-		}
+		if (*str == (unsigned char)c)
+			return ((void *)str);
+		str++;
 		i++;
 	}
 	return (NULL);
