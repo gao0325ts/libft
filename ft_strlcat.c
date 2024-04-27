135608921c0a807b5a76b546dd57078acb5eaab0
stakada <ts20040325@gmail.com>
2024-04-27 23:35:25 +0900

    fix strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 03:30:08 by stakada           #+#    #+#             */
-/*   Updated: 2024/04/20 17:04:04 by stakada          ###   ########.fr       */
+/*   Updated: 2024/04/27 23:33:18 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,17 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	src_len;
 	size_t	n;
 
-	dst_len = ft_strlen(dst);
+	dst_len = 0;
 	src_len = ft_strlen(src);
 	n = 0;
 	if (dstsize == 0 || dst == NULL)
 		return src_len;
-	else if (dstsize <= dst_len)
+	dst_len = ft_strlen(dst);
+	if (dstsize <= dst_len)
 		return (dstsize + src_len);
 	else
 	{
+		dst_len = ft_strlen(dst);
 		while (*dst != '\0')
 			dst++;
 		while (*src != '\0' && n < dstsize - dst_len - 1)
