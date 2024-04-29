6dbf513189b0c09c09a00dbbfa9969ee146797d1
stakada <ts20040325@gmail.com>
2024-04-29 19:04:51 +0900

    fix strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,13 +6,11 @@
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 03:44:44 by stakada           #+#    #+#             */
-/*   Updated: 2024/04/29 19:04:16 by stakada          ###   ########.fr       */
+/*   Updated: 2024/04/29 19:04:47 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-#include <stdio.h>
 
 char	*ft_strrchr(const char *s, int c)
 {
@@ -29,8 +27,10 @@ char	*ft_strrchr(const char *s, int c)
 	return ((char *)p);
 }
 
-int main(void)
-{
-	printf("%s\n", ft_strrchr("teste", 1024));
-	return 0;
-}
+// #include <stdio.h>
+
+// int main(void)
+// {
+// 	printf("%s\n", ft_strrchr("teste", 1024));
+// 	return 0;
+// }
