bd69205067eb299d0f8f924bc478c431ff6c80f5
stakada <ts20040325@gmail.com>
2024-04-29 14:22:39 +0900

    itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,13 +6,78 @@
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 13:14:37 by stakada           #+#    #+#             */
-/*   Updated: 2024/04/29 13:18:51 by stakada          ###   ########.fr       */
+/*   Updated: 2024/04/29 14:21:58 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+#include <stdio.h>
+
+static char *ft_itoa_int_min(int n)
+{
+  long num;
+  int len;
+  char *result;
+
+  num = n;
+  num = -num;
+  len = 11;
+  result = (char *)malloc(sizeof(char) * (len + 1));
+  if (!result)
+    return (NULL);
+  result[len] = '\0';
+  while(num != 0)
+  {
+    result[--len] = num % 10 + '0';
+    num /= 10;
+  }
+  result[0] = '-';
+  return (result);
+}
+
+static int count_len(int n, int isNegative)
+{
+  int len;
+
+  len = 0;
+  if (n == 0)
+    return (1);
+  else if (isNegative)
+    len++;
+  while (n != 0)
+  {
+    len++;
+    n /= 10;
+  }
+  return (len);
+}
+
 char *ft_itoa(int n)
 {
-  
+  char *str;
+  int isNegative;
+  int len;
+
+  isNegative = 0;
+  if (n == INT_MIN)
+    return (ft_itoa_int_min(n));
+  if (n < 0)
+  {
+    isNegative = 1;
+    n = -n;
+  }
+  len = count_len(n, isNegative);
+  str = (char *)malloc(sizeof(char) * (len + 1));
+  if (!str)
+    return (NULL);
+  str[len--] = '\0';
+  while (len >= 0)
+  {
+    str[len--] = n % 10 + '0';
+    n /= 10;
+  }
+  if (isNegative)
+    str[0] = '-';
+  return (str);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 21:34:40 by stakada           #+#    #+#             */
-/*   Updated: 2024/04/29 13:13:36 by stakada          ###   ########.fr       */
+/*   Updated: 2024/04/29 13:20:24 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,5 +46,6 @@ char *ft_substr(char const *s, unsigned int start, size_t len);
 char *ft_strjoin(char const *s1, char const *s2);
 char *ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *str, char c);
+char *ft_itoa(int n);
 
 #endif
