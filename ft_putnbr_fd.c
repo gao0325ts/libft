533cc2562af1c7a1d8ce109739c180f2f87aa213
gao0325ts <160503699+gao0325ts@users.noreply.github.com>
2024-05-06 16:15:59 +0900

    Update ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -14,22 +14,19 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	long	nbr;
-
-	nbr = n;
-	if (nbr == -2147483648)
+	if (n == -2147483648)
 	{
 		write(fd, "-2147483648", 11);
 		return ;
 	}
-	if (nbr < 0)
+	if (n < 0)
 	{
 		write(fd, "-", 1);
-		nbr = -nbr;
+		n = -n;
 	}
-	if (nbr >= 10)
-		ft_putnbr_fd(nbr / 10, fd);
-	ft_putchar_fd(nbr % 10 + '0', fd);
+	if (n >= 10)
+		ft_putnbr_fd(n / 10, fd);
+	ft_putchar_fd(n % 10 + '0', fd);
 }
 
 // #include <stdio.h>
