b3cdbe233143935d4833823e9ae3ce6c06fa4d31
stakada <stakada@student.42tokyo.jp>
2024-05-17 10:34:12 +0900

    update split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 02:24:51 by stakada           #+#    #+#             */
-/*   Updated: 2024/05/03 12:11:36 by stakada          ###   ########.fr       */
+/*   Updated: 2024/05/17 10:33:42 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static size_t	word_length(const char *str, char c)
 	size_t	len;
 
 	len = 0;
-	while (str[len] != '\0' && str[len] != c)
+	while (str[len] && str[len] != c)
 		len++;
 	return (len);
 }
@@ -27,14 +27,14 @@ static size_t	count_words(const char *str, char c)
 	size_t	count;
 
 	count = 0;
-	while (*str != '\0')
+	while (*str)
 	{
-		while (*str != '\0' && *str == c)
+		while (*str && *str == c)
 			str++;
-		if (*str != '\0')
+		if (*str)
 		{
 			count++;
-			while (*str != '\0' && *str != c)
+			while (*str && *str != c)
 				str++;
 		}
 	}
@@ -51,11 +51,11 @@ static char	**ft_split_support(char **tmp, char const *str, char c,
 	i = 0;
 	while (i < word_count)
 	{
-		while (*str != '\0' && *str == c)
+		while (*str && *str == c)
 			str++;
 		len = word_length(str, c);
 		tmp[i] = (char *)malloc(sizeof(char) * (len + 1));
-		if (tmp[i] == NULL)
+		if (!tmp[i])
 		{
 			while (i--)
 				free(tmp[i]);
@@ -76,14 +76,14 @@ char	**ft_split(char const *str, char c)
 	size_t	word_count;
 	char	**result;
 
-	if (str == NULL)
+	if (!str)
 		return (NULL);
 	word_count = count_words(str, c);
 	result = (char **)malloc(sizeof(char *) * (word_count + 1));
-	if (result == NULL)
+	if (!result)
 		return (NULL);
 	result = ft_split_support(result, str, c, word_count);
-	if (result == NULL)
+	if (!result)
 		return (NULL);
 	result[word_count] = NULL;
 	return (result);
