2a555d1bcb7417edfd40125f133565274d923c4c
stakada <stakada@student.42tokyo.jp>
2024-05-17 10:35:14 +0900

    update strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 23:19:54 by stakada           #+#    #+#             */
-/*   Updated: 2024/05/02 04:00:40 by stakada          ###   ########.fr       */
+/*   Updated: 2024/05/17 10:34:52 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ char	*ft_strdup(const char *s1)
 	char	*new_str;
 
 	new_str = (char *)malloc(ft_strlen(s1) + 1);
-	if (new_str == NULL)
+	if (!new_str)
 		return (NULL);
 	start = new_str;
-	while (*s1 != '\0')
+	while (*s1)
 		*new_str++ = *s1++;
 	*new_str = '\0';
 	return (start);
