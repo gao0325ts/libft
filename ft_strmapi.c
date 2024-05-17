86a137db7731d4ce41740fe26d04a732ce7fcf5f
stakada <stakada@student.42tokyo.jp>
2024-05-17 10:40:09 +0900

    update strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 14:40:50 by stakada           #+#    #+#             */
-/*   Updated: 2024/05/03 22:07:20 by stakada          ###   ########.fr       */
+/*   Updated: 2024/05/17 10:39:48 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	unsigned int	i;
 	char			*result;
 
-	if (s == NULL || f == NULL)
+	if (!s || !f)
 		return (NULL);
 	result = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
-	if (result == NULL)
+	if (!result)
 		return (NULL);
 	i = 0;
-	while (s[i] != '\0')
+	while (s[i])
 	{
 		result[i] = f(i, s[i]);
 		i++;
