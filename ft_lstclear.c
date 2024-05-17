9fcc74ea62bd0da39afb890ce09d6426d5c743ee
stakada <stakada@student.42tokyo.jp>
2024-05-17 10:20:27 +0900

    update lstclear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 17:32:15 by stakada           #+#    #+#             */
-/*   Updated: 2024/05/02 06:15:47 by stakada          ###   ########.fr       */
+/*   Updated: 2024/05/17 10:20:04 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*next;
 
-	if (lst == NULL || del == NULL)
+	if (!lst || !del)
 		return ;
-	while (*lst != NULL)
+	while (*lst)
 	{
 		next = (*lst)->next;
 		ft_lstdelone(*lst, del);
